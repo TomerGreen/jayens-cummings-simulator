0354cfc7b5120f0c81b047432f4af589ce1b2cba
tomerg <tomerg@cs.huji.ac.il>
2022-01-25 23:32:35 +0200

    Finished time evolution for case 1 + 2/3 of the plots

--- hilberSpaceOfCombinedSys.py
+++ hilberSpaceOfCombinedSys.py
@@ -1,30 +1,21 @@
 import numpy as np
-# from scipy.linalg import expm
+from scipy.linalg import logm
 from qutip import *
+from typing import Iterable
+import matplotlib.pyplot as plt
 
 
-#  ==== Params ==== #
-N = 5  # max num of photons is N-1
+N = 100  # max num of photons is N-1
 ALPHA = 7
-WC = 6 * 2 * np.pi * 10e9  # cavity frequency
-WA = 6 * 2 * np.pi * 10e9  # atom frequency
-G = 15 * 2 * np.pi * 10e6   # coupling strength
-DT = 0.2 * 10e-9
+WC = 6 * 2 * np.pi * 1e9  # cavity frequency
+WA = 6 * 2 * np.pi * 1e9  # atom frequency
+G = 15 * 2 * np.pi * 1e6   # coupling strength
+DT = 0.2e-9
+TOT_TIME = 1.6e-6
+STEPS = 8000
 
 
-# === CONSTANTS === #
-X = np.array([[0, 1], [1, 0]])
-Y = np.array([[0, -1j], [1j, 0]])
-Z = np.array([[1, 0], [0, -1]])
-SM = (X - 1j * Y) / 2
-SP = (X + 1j * Y) / 2
-A = np.zeros((N, N))
-for i in range(1, N):
-    A[i - 1, i] = np.sqrt(i)
-A_DAG = A.conj().T
-
-
-def jc_ham(wc=WC, wa=WA, g=G):
+def jc_ham(wc=WC, wa=WA, g=G) -> Qobj:
     """Returns the Jaynes-Cummings Hamiltonian."""
     ham = wc * tensor(create(N) * destroy(N), identity(2))
     ham += (1 / 2) * wa * tensor(identity(N), sigmaz())
@@ -32,7 +23,7 @@ def jc_ham(wc=WC, wa=WA, g=G):
     return ham
 
 
-def evolve_state(init_state: Qobj, ham: Qobj, steps, dt=DT):
+def evolve_state(init_state: Qobj, ham: Qobj, steps, dt=DT) -> list:
     """
     Evolves an initial state in time according to a given Hamiltonian and returns an array describing the state
     after each time step.
@@ -43,13 +34,12 @@ def evolve_state(init_state: Qobj, ham: Qobj, steps, dt=DT):
     psi = init_state
     psi_t = list()
     for step in range(steps):
-        print(psi)
         psi = time_evo_op * psi
         psi_t.append(psi)
     return psi_t
 
 
-def init_state(alpha=ALPHA):
+def init_state(alpha=ALPHA) -> Qobj:
     """Returns the initial two-system state"""
     bos_vac = basis(N)
     disp_op = Qobj.expm(alpha * create(N) - alpha.conjugate() * destroy(N))
@@ -59,7 +49,33 @@ def init_state(alpha=ALPHA):
     return psi0
 
 
+def draw_excited_state_population(psi_t: Iterable, steps=STEPS, dt=DT):
+    """Draws the excited state population figure."""
+    rdms = [state.ptrace(1) for state in psi_t]
+    ee_pop = [rho[0, 0] for rho in rdms]
+    times = np.linspace(0.0, steps * dt, steps)
+    plt.plot(times, ee_pop, linewidth=0.5)
+    plt.xlabel("Time (s)")
+    plt.ylabel("Excited TLS Population")
+    plt.suptitle(r'Two-Level System Excited State Population for $\alpha=$' + str(ALPHA))
+    plt.show()
+
+
+def draw_purity_plot(psi_t: Iterable, steps=STEPS, dt=DT):
+    """Draws the purity figure"""
+    rdms = [state.ptrace(1).data.toarray() for state in psi_t]
+    # purities = [1-np.trace(np.dot(rdm, logm(rdm))) for rdm in rdms]
+    purities = [np.trace(np.dot(rdm, rdm)) for rdm in rdms]
+    times = np.linspace(0.0, steps * dt, steps)
+    plt.plot(times, purities, linewidth=0.7)
+    plt.xlabel("Time (s)")
+    plt.ylabel(r'$Tr(\rho_{TLS}^2)$')
+    plt.suptitle(r'Purity of Combined State for $\alpha=$' + str(ALPHA))
+    plt.show()
+
+
 if __name__ == '__main__':
     H = jc_ham()
     psi0 = init_state()
-    psi_t = evolve_state(psi0, H, 8000, DT)
+    psi_t = evolve_state(psi0, H, STEPS)
+    draw_purity_plot(psi_t)
