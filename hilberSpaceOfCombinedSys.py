1ac49bafbf899bbcb39b0ce747fda590267fe0af
ofekvardi <ofekvardi@gmail.com>
2022-01-26 15:53:01 +0200

    with case 2

--- hilberSpaceOfCombinedSys.py
+++ hilberSpaceOfCombinedSys.py
@@ -4,18 +4,21 @@ from qutip import *
 from typing import Iterable
 import matplotlib.pyplot as plt
 
-
 N = 100  # max num of photons is N-1
 ALPHA = 7
 WC = 6 * 2 * np.pi * 1e9  # cavity frequency
-WA = 6 * 2 * np.pi * 1e9  # atom frequency
-G = 15 * 2 * np.pi * 1e6   # coupling strength
+# WA_1 = 6 * 2 * np.pi * 1e9  # atom frequency for case 1
+# WA_2 = 5 * 2 * np.pi * 1e9  # atom frequency for case 2
+G = 15 * 2 * np.pi * 1e6  # coupling strength
 DT = 0.2e-9
 TOT_TIME = 1.6e-6
 STEPS = 8000
 
+case_1 = {"WA": 6 * 2 * np.pi * 1e9, "TOT_TIME": 1.6e-6, "STEPS": 8000, "DT": 0.2e-9}
+case_2 = {"WA": 5 * 2 * np.pi * 1e9, "TOT_TIME": 4e-6, "STEPS": 4000, "DT": 1e-9}
 
-def jc_ham(wc=WC, wa=WA, g=G) -> Qobj:
+
+def jc_ham(wc=WC, wa=case_2["WA"], g=G) -> Qobj:
     """Returns the Jaynes-Cummings Hamiltonian."""
     ham = wc * tensor(create(N) * destroy(N), identity(2))
     ham += (1 / 2) * wa * tensor(identity(N), sigmaz())
@@ -23,7 +26,7 @@ def jc_ham(wc=WC, wa=WA, g=G) -> Qobj:
     return ham
 
 
-def evolve_state(init_state: Qobj, ham: Qobj, steps, dt=DT) -> list:
+def evolve_state(init_state: Qobj, ham: Qobj, steps=case_2["STEPS"], dt=case_2["DT"]) -> list:
     """
     Evolves an initial state in time according to a given Hamiltonian and returns an array describing the state
     after each time step.
@@ -49,11 +52,16 @@ def case1_init_state(alpha=ALPHA) -> Qobj:
     return psi0
 
 
-def case2_init_state():
-    pass
+def case2_init_state(alpha=ALPHA) -> Qobj:
+    bos_vac = basis(N)
+    disp_op = Qobj.expm(alpha * create(N) - alpha.conjugate() * destroy(N))
+    bos_coher = disp_op * bos_vac
+    tls_sup = (1/np.sqrt(2)) * (sigmam() * basis(2) + basis(2))
+    psi0 = tensor(bos_coher, tls_sup)
+    return psi0
 
 
-def draw_excited_state_population(psi_t: Iterable, steps=STEPS, dt=DT):
+def draw_excited_state_population(psi_t: Iterable, steps=case_2["STEPS"], dt=case_2["DT"]):
     """Draws the excited state population figure."""
     rdms = [state.ptrace(1) for state in psi_t]
     ee_pop = [rho[0, 0] for rho in rdms]
@@ -65,7 +73,7 @@ def draw_excited_state_population(psi_t: Iterable, steps=STEPS, dt=DT):
     plt.show()
 
 
-def draw_purity_plot(psi_t: Iterable, steps=STEPS, dt=DT):
+def draw_purity_plot(psi_t: Iterable, steps=case_2["STEPS"], dt=case_2["DT"]):
     """Draws the purity figure"""
     rdms = [state.ptrace(1).data.toarray() for state in psi_t]
     # purities = [1-np.trace(np.dot(rdm, logm(rdm))) for rdm in rdms]
@@ -78,12 +86,22 @@ def draw_purity_plot(psi_t: Iterable, steps=STEPS, dt=DT):
     plt.show()
 
 
-def draw_bosonic_state_density():
-    pass
+def draw_bosonic_state_density(psi_t: Iterable,  steps=case_2["STEPS"], dt=case_2["DT"]):
+    psi = np.array(psi_t)
+    print(psi)
+    state_dens = [st * np.conjugate(st) for st in psi]
+    times = np.linspace(0.0, steps * dt, steps) * np.ones_like(state_dens)
+    states = np.linspace(0.0, len(state_dens)) * np.ones_like(state_dens)
+    # print(state_dens)
+    # print(times.shape)
+    # print(states.shape)
+    plt.pcolormesh(times, states, state_dens, cmap='hot')
+    plt.show()
 
 
 if __name__ == '__main__':
     H = jc_ham()
-    psi0 = case1_init_state()
-    psi_t = evolve_state(psi0, H, STEPS)
-    draw_purity_plot(psi_t)
+    psi0 = case2_init_state()
+    psi_t = evolve_state(psi0, H, case_2["STEPS"])
+    # draw_purity_plot(psi_t)
+    draw_bosonic_state_density(psi_t)
